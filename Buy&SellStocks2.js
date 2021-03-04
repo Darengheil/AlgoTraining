4d8aa0d8ad99cff5636b1b13054072ae52813abe
Einhart <einhvas@gmail.com>
2021-03-05 01:28:53 +0300

    Solved pretty simple task with stocks

---
+++ Buy&SellStocks2.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let profit = 0;
+    for(let i = 0; i < prices.length; i++) {
+        if(prices[i]<prices[i+1]) {
+            profit += (prices[i+1] - prices[i]);
+        }
+    }
+    return profit;
+};
+
+console.log(maxProfit([7,1,5,3,6,4]));
+console.log(maxProfit([1,2,3,4,5]));
+console.log(maxProfit([7,6,4,3,1]));
