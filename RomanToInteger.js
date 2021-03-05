9a07312e489b2a5420842f409eb7e9db99ebe26e
Einhart <einhvas@gmail.com>
2021-03-05 04:34:52 +0300

    Solved simple task with sorting arrays

---
+++ RomanToInteger.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    
+};
+
+console.log(romanToInt("III"));
+console.log(romanToInt("IV"));
+console.log(romanToInt("IX"));
+console.log(romanToInt("LVIII"));
+console.log(romanToInt("MCMXCIV"));
