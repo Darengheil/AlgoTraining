c679b2a5ea567a1b8d585b035c8e3d070ca7073f
Darengheil <einhvas@gmail.com>
2021-03-13 23:36:06 +0300

    Brutforced algos with floating point xD

--- powerofthree.js
+++ powerofthree.js
@@ -3,22 +3,18 @@
  * @return {boolean}
  */
  var isPowerOfThree = function(n) {
-    let log = Math.log(n)/Math.log(3);
-    if (log === parseInt(log, 10))
-        console.log("data is integer")
-    else
-        console.log("data is not an integer")
-    return (Math.log(n)/Math.log(3));
+    return (parseFloat(Math.log(n)/Math.log(3)).toPrecision(14))%1 === 0;
 };
 
-console.log(isPowerOfThree(27));
-console.log(isPowerOfThree(0));
-console.log(isPowerOfThree(9));
-console.log(isPowerOfThree(45));
-console.log(isPowerOfThree(-3));
-console.log(isPowerOfThree(1));
+console.log(isPowerOfThree(27)); //true
+console.log(isPowerOfThree(0)); //false
+console.log(isPowerOfThree(9)); //true
+console.log(isPowerOfThree(45)); //false
+console.log(isPowerOfThree(-3)); //false
+console.log(isPowerOfThree(1)); //true
+console.log(isPowerOfThree(19682)); // false
+console.log(isPowerOfThree(81)); // true
 
 /*
-
-    
+    return (parseFloat(Math.log(n)/Math.log(3)).toPrecision(10))%1 === 0;    
 */
