3ca4d27129c586fc97443c39051638cbaa43d5d8
Darengheil <einhvas@gmail.com>
2021-03-17 02:39:51 +0300

    Added solution for finding prefix in string

--- LongestCommPrefix.js
+++ LongestCommPrefix.js
@@ -9,11 +9,14 @@
      let firststr = strs[0];
      for(let i = 1; i < strs.length; i++) {
          while(strs[i].indexOf(firststr)!== 0){
-            
+            firststr = firststr.substr(0, firststr.length - 1);
+            if(firststr === 0){
+                return "";
+            }
          }
      }
+     return firststr;
 };
 
-console.log(strStr(["flower","flow","flight"]));// "f1"
-console.log(strStr(["dog","racecar","car"]));// ""
-// TODO
+console.log(longestCommonPrefix(["flower","flow","flight"]));// "f1"
+console.log(longestCommonPrefix(["dog","racecar","car"]));// ""
