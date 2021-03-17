927a17b0227fad39b2da57a48f0c9fe5dbeadb1d
Darengheil <einhvas@gmail.com>
2021-03-17 04:39:08 +0300

    Later

---
+++ PascalTriangle.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+ var generate = function(numRows) {
+    let answ = [];
+    let temp = [];
+    while(temp.length<=numRows){
+        temp.push(1);
+        console.log(temp);
+    }
+    return answ;
+};
+
+
+console.log(generate(5)); //[[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+// console.log(generate(1)); //[[1]]
+// TODO
