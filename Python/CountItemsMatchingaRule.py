7c312eda76329429408195a7003492067e0a1947
Einhart <einhvas@gmail.com>
2021-09-11 17:06:13 +0300

    TODO

---
+++ Python/CountItemsMatchingaRule.py
@@ -0,0 +1,13 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        
+        print()
+        return 
+
+
+
+answer = Solution()
+answer.countMatches("RLRRLLRLRL")
+# answer.countMatches("RLLLLRRRLR")
+# answer.countMatches("LLLLRRRR")
+# answer.countMatches("RLRRRLLRLL")
