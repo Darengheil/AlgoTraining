be26d87916bbe3324055958f9ae74709aedca810
Einhart <einhvas@gmail.com>
2021-09-18 02:08:12 +0300

    perebor

--- Python/TwoSum.py
+++ Python/TwoSum.py
@@ -1,19 +1,21 @@
 from typing import List
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        answ = []
-        for i in nums:
-            for j in nums:
-                if ((i + j) == target) and nums[i] != nums[j]:
-                    answ.append(nums.index(i))
-                    answ.append(nums.index(j))
-        # del answ[2:]
-        print(answ)
-        return answ
-
+        l = 0
+        r = len(nums) - 1
+        while l < r:
+            sum = nums[l] + nums[r]
+            if sum == target:
+                print([l, r])
+                return[l, r]
+            r -= 1
+        l += 1
+        return[]
 
 
 answer = Solution()
-answer.twoSum([2,7,11,15], 9)
-answer.twoSum([3,2,4], 6)
-answer.twoSum([3,3], 6)
+answer.twoSum([2, 7, 11, 15], 9)
+answer.twoSum([3, 2, 4], 6)
+answer.twoSum([3, 3], 6)
