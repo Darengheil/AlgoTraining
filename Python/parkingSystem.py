51d93b4d082fc4359fec12730c5cc70a2bec97f5
Einhart <einhvas@gmail.com>
2021-10-14 12:49:07 +0300

    Later

---
+++ Python/parkingSystem.py
@@ -0,0 +1,15 @@
+from typing import List
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        
+
+    def addCar(self, carType: int) -> bool:
+        
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
+
+answer = ParkingSystem()
